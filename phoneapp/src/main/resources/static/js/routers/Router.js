6067bfdce0db13dfa09bc6e6f21fd5939bfc8d30
natasa-kulevanova <n.kulevanova@yahoo.com>
2017-02-05 23:31:13 +0100

    small UI changes

--- phoneapp/src/main/resources/static/js/routers/Router.js
+++ phoneapp/src/main/resources/static/js/routers/Router.js
@@ -9,11 +9,13 @@ app.Router = Backbone.Router.extend({
     showFirstPage: function(){
         $("#phonePreviewSection").hide();
         $("#allPhones").show();
+        $("#filters").show();
     },
     
     previewPhone: function(phoneLink){
         var previewModel = _.first(phonesGroup.where({link: phoneLink}));
         $("#allPhones").hide();
+        $("#filters").hide();
         var pPreview = new app.phonePreviewView({model : previewModel});
         $("#phonePreviewSection").html(pPreview.render().el);
         $("#phonePreviewSection").show();
