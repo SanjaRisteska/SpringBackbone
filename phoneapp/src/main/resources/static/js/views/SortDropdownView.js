7fa5e3e9f6891702dfc9afee6c05bc8f03a6624c
natasa-kulevanova <n.kulevanova@yahoo.com>
2017-03-05 20:32:33 +0100

    remove duplicate code

--- phoneapp/src/main/resources/static/js/views/SortDropdownView.js
+++ phoneapp/src/main/resources/static/js/views/SortDropdownView.js
@@ -23,48 +23,6 @@ app.sortDropdownView = Backbone.View.extend({
     },
     
     sortPhones: function(){
-    	var sortedPhones = [];
-    	var sort = $("#sort-dropdown option:selected").val();
-    	var filterBy = $("#filter-dropdown option:selected").val();
-    	
-    	switch(sort){
-    	case "model_ascending":
-    		sortedPhones = phonesGroup.sortBy(function(phone){
-    			return phone.get("model").toLowerCase();
-    		});
-    		break;
-    	case "model_descending":
-    		sortedPhones = phonesGroup.sortBy(function(phone){
-    			return phone.get("model").toLowerCase();
-    		});
-    		sortedPhones.reverse();
-    		break;
-    	case "price_ascending":
-    		sortedPhones = phonesGroup.sortBy(function(phone){
-    			return phone.get("price");
-    		});
-    		break;
-    	case "price_descending":
-    		sortedPhones = phonesGroup.sortBy(function(phone){
-    			return phone.get("price");
-    		});
-    		sortedPhones.reverse();
-    		break;
-    	case "all": 
-    		sortedPhones = phonesGroup.models;
-    		break;
-    	}
-    	
-    	var filteredPhones = _.filter(sortedPhones, function(model){
-    		return model.get("manufacturer").toLowerCase() === filterBy;
-    	});
-    	
-    	if(filterBy === 'all' && filteredPhones.length == 0){
-    		filteredPhones = sortedPhones;
-    	}
-    	
-    	var modifiedPhonesGroup = new app.phonesCollection(filteredPhones);
-    	phonesGroupView.postLayoutRender(modifiedPhonesGroup);
-    	
+    	$("#filter-dropdown").trigger("change");
     }
 });
